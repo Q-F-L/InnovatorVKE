0d3973aa33e6bc84672c7ab88715a9f34ffa6e4e
Q-F-L <doshmosh@yandex.ru>
2022-05-29 13:48:04 +0300

    create visibel message

--- lib/Pages/massage_page.dart
+++ lib/Pages/massage_page.dart
@@ -17,7 +17,6 @@ class _MassagetPage extends State<MassagetPage> {
     discussion = getDiscussionList();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,7 +63,112 @@ class _MassagetPage extends State<MassagetPage> {
                         future: discussion,
                         builder: (context, snapshot) {
                           if (snapshot.hasData) {
-                            return Text('dsad');
+                            return ListView.builder(
+                              itemCount: snapshot.data?.discussion.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                if (5 != snapshot.data?.discussion[index].user_id)
+                                  return Container(
+                                    alignment: Alignment.bottomLeft,
+                                    margin: EdgeInsets.only(bottom: 20),
+                                    padding: EdgeInsets.only(
+                                        bottom: 20,
+                                        top: 10,
+                                        left: 15,
+                                        right: 15),
+                                    decoration: BoxDecoration(
+                                      color: Color.fromRGBO(238, 240, 242, 1),
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(5),
+                                        bottomLeft: Radius.circular(20),
+                                        topRight: Radius.circular(20),
+                                        bottomRight: Radius.circular(20),
+                                      ),
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Container(
+                                          alignment: Alignment.bottomLeft,
+                                          child: Text(
+                                            '${snapshot.data?.discussion[index].user_id}',
+                                            style: TextStyle(
+                                              fontSize: 14,
+                                              color: Color.fromRGBO(
+                                                  161, 161, 161, 1),
+                                            ),
+                                            textAlign: TextAlign.left,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 5,
+                                        ),
+                                        Container(
+                                          alignment: Alignment.bottomLeft,
+                                          child: Text(
+                                            '${snapshot.data?.discussion[index].message}',
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              color: Colors.black,
+                                            ),
+                                            textAlign: TextAlign.start,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                else
+                                  return Container(
+                                    alignment: Alignment.bottomRight,
+                                    margin: EdgeInsets.only(bottom: 20),
+                                    padding: EdgeInsets.only(
+                                        bottom: 20,
+                                        top: 10,
+                                        left: 15,
+                                        right: 15),
+                                    decoration: BoxDecoration(
+                                      color: Color.fromRGBO(32, 86, 146, 1),
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(20),
+                                        bottomLeft: Radius.circular(20),
+                                        topRight: Radius.circular(5),
+                                        bottomRight: Radius.circular(20),
+                                      ),
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Container(
+                                          alignment: Alignment.bottomRight,
+                                          child: Text(
+                                            '${snapshot.data?.discussion[index].user_id}',
+                                            style: TextStyle(
+                                              fontSize: 14,
+                                              color: Color.fromRGBO(
+                                                  161, 161, 161, 1),
+                                            ),
+                                            textAlign: TextAlign.left,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 5,
+                                        ),
+                                        Container(
+                                          alignment: Alignment.bottomRight,
+                                          child: Text(
+                                            '${snapshot.data?.discussion[index].message}',
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              color: Colors.white,
+                                            ),
+                                            textAlign: TextAlign.start,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                              },
+                            );
                           } else if (snapshot.hasError) {
                             print(snapshot);
                             return Text('${snapshot}');
